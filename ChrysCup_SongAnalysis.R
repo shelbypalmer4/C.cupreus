85cdb6bc3590b71daa0d4ad3d9fd7717774fe21a
shelbypalmer4 <Palmer904@live.missouristate.edu>
2023-09-10 13:44:20 -0400

    add quality check 1 figures

--- ChrysCup_SongAnalysis.R
+++ ChrysCup_SongAnalysis.R
@@ -269,5 +269,40 @@ files <- list.files()
 lapply(files, FUN=filt_resamp)
 
 
+#### 10 September 2023: Create spectrograms with timer() overlay for recording quality check and timer() amplitude threshold determination ####
+
+# set working directory to the location of the sound files
+setwd("C:/Users/Shelby Palmer/Desktop/C.cupreus/filter_resamp")
+head(list.files())
+
+# manually make a new folder in the working directory for the figures named "QC_10_figs"
+
+# function for generating images
+cutspec2 <- function(x) {
+  a <- readWave(x)
+  png(filename = paste(getwd(), "/QC1_figs/", x, ".png", sep = ""),
+      width = 800,
+      height = 300)
+  spectro(a, 
+          wl = 512, 
+          ovlp = 95, 
+          collevels = seq(-42,0,6),
+          flim = c(0, 7),
+          osc = F, 
+          scale = F, 
+          colgrid = "gray", 
+          cexlab = 0.8,
+          cexaxis = 0.7)
+  par(new=T)
+  try(timer(a, 
+            dmin = 0.05,
+            envt = "hil",
+            msmooth=c(512, 90),
+            threshold = 10))
+  dev.off()
+}
+# apply over working directory
+lapply(list.files(pattern = ".wav"), cutspec2)
+
 
 
