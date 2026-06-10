35b8286d2d8ea8f15012ccf6f023a418652f44a4
Shelby <Palmer904@live.missouristate.edu>
2026-06-09 22:18:48 -0400

    adding additional recordings to dataset

--- cuckoo_data_sims_SMP.R
+++ cuckoo_data_sims_SMP.R
@@ -6,7 +6,7 @@ setwd("C:/Users/spalm/OneDrive - University of Florida/Desktop/C.cupreus")
 simsdf <- read.csv("simsdf_full_crit.csv")
 # unique numbers of songs produced
 # n_songs <- c(1:12,14,15,18,22,29)
-nsongsmd <- read.csv("Curated_Songs_metadata_04Jun2026.csv")
+nsongsmd <- read.csv("Curated_Songs_metadata_09Jun2026.csv")
 nrec <- sort(nsongsmd$Songs)
 # number of recordings for which the corresponding n_songs is true
 # number_of_samp_size_occurrence <- c(8,9,10,7,17,12,7,6,6,4,3,3,1,1,1,1,1)
